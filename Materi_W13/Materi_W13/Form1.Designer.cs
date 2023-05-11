114d05c13dd2185474ff5cc7fddb3626802ce012
Livanty <121992352+Livanty@users.noreply.github.com>
2023-05-11 11:51:29 +0700

    CA_W13

---
+++ Materi_W13/Materi_W13/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Materi_W13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txb1 = new System.Windows.Forms.TextBox();
+            this.txb2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txb1
+            // 
+            this.txb1.Location = new System.Drawing.Point(40, 52);
+            this.txb1.Name = "txb1";
+            this.txb1.Size = new System.Drawing.Size(100, 20);
+            this.txb1.TabIndex = 0;
+            // 
+            // txb2
+            // 
+            this.txb2.Location = new System.Drawing.Point(40, 171);
+            this.txb2.Name = "txb2";
+            this.txb2.Size = new System.Drawing.Size(230, 20);
+            this.txb2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txb2);
+            this.Controls.Add(this.txb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txb1;
+        private System.Windows.Forms.TextBox txb2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Materi_W13/Materi_W13/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace Materi_W13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string coba = txb1.Text;
+            MD5 md5= new MD5CryptoServiceProvider();
+            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(coba));
+            byte[] result = md5.Hash;
+
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                stringBuilder.Append(result[i].ToString("x2"));
+            }
+            txb2.Text = (stringBuilder.ToString());
+        }
+    }
+}
