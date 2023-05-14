4480172555e7cc535726ca2d5dd4691c0f75f34d
Livanty <121992352+Livanty@users.noreply.github.com>
2023-05-14 21:58:23 +0700

    CA_W12

---
+++ CA_W12/CA_W12/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace CA_W12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.tbox1 = new System.Windows.Forms.TextBox();
+            this.tbox2 = new System.Windows.Forms.TextBox();
+            this.tbox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbox4 = new System.Windows.Forms.TextBox();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.tbox7 = new System.Windows.Forms.TextBox();
+            this.tbox6 = new System.Windows.Forms.TextBox();
+            this.tbox5 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(256, 31);
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersWidth = 62;
+            this.dgv.RowTemplate.Height = 28;
+            this.dgv.Size = new System.Drawing.Size(493, 388);
+            this.dgv.TabIndex = 0;
+            this.dgv.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_CellClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(75, 132);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "button1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click_1);
+            // 
+            // tbox1
+            // 
+            this.tbox1.Location = new System.Drawing.Point(118, 24);
+            this.tbox1.Name = "tbox1";
+            this.tbox1.Size = new System.Drawing.Size(100, 26);
+            this.tbox1.TabIndex = 3;
+            // 
+            // tbox2
+            // 
+            this.tbox2.Location = new System.Drawing.Point(118, 56);
+            this.tbox2.Name = "tbox2";
+            this.tbox2.Size = new System.Drawing.Size(100, 26);
+            this.tbox2.TabIndex = 4;
+            // 
+            // tbox3
+            // 
+            this.tbox3.Location = new System.Drawing.Point(118, 88);
+            this.tbox3.Name = "tbox3";
+            this.tbox3.Size = new System.Drawing.Size(100, 26);
+            this.tbox3.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "ID";
+            // 
+            // tbox4
+            // 
+            this.tbox4.Location = new System.Drawing.Point(118, 179);
+            this.tbox4.Name = "tbox4";
+            this.tbox4.Size = new System.Drawing.Size(100, 26);
+            this.tbox4.TabIndex = 7;
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(75, 224);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 8;
+            this.btn2.Text = "button2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // tbox7
+            // 
+            this.tbox7.Location = new System.Drawing.Point(118, 346);
+            this.tbox7.Name = "tbox7";
+            this.tbox7.Size = new System.Drawing.Size(100, 26);
+            this.tbox7.TabIndex = 12;
+            // 
+            // tbox6
+            // 
+            this.tbox6.Location = new System.Drawing.Point(118, 314);
+            this.tbox6.Name = "tbox6";
+            this.tbox6.Size = new System.Drawing.Size(100, 26);
+            this.tbox6.TabIndex = 11;
+            // 
+            // tbox5
+            // 
+            this.tbox5.Location = new System.Drawing.Point(118, 282);
+            this.tbox5.Name = "tbox5";
+            this.tbox5.Size = new System.Drawing.Size(100, 26);
+            this.tbox5.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 289);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "ID";
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(75, 388);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 23);
+            this.btn3.TabIndex = 13;
+            this.btn3.Text = "button3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Nation";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 20);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Abbreviation";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 349);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(97, 20);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Abbreviation";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 314);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 20);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Nation";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.tbox7);
+            this.Controls.Add(this.tbox6);
+            this.Controls.Add(this.tbox5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.tbox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbox3);
+            this.Controls.Add(this.tbox2);
+            this.Controls.Add(this.tbox1);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgv);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.TextBox tbox1;
+        private System.Windows.Forms.TextBox tbox2;
+        private System.Windows.Forms.TextBox tbox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbox4;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.TextBox tbox7;
+        private System.Windows.Forms.TextBox tbox6;
+        private System.Windows.Forms.TextBox tbox5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ CA_W12/CA_W12/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CA_W12
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection sqlConnect;
+        MySqlCommand sqlCommand;
+        MySqlDataAdapter sqlAdapter;
+        MySqlDataReader sqlDataReader;
+        string connectionString;
+        string sqlQuery;
+        DataTable dt = new DataTable();
+        public Form1()
+        {
+
+            try
+            {
+                connectionString = "server=localhost;uid=root;pwd=0mR0Nh3Mc4b!;database=premier_league";
+                sqlConnect = new MySqlConnection(connectionString);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            InitializeComponent();
+            dgv.DataSource = dt;
+            tbox4.Enabled = false;
+            tbox5.Enabled = false;
+        }
+       
+       
+        private void updateDGV1()
+        {
+            dt.Clear();
+            try
+            {
+                string command = "select * from nationality";
+                sqlCommand = new MySqlCommand(command, sqlConnect);
+                sqlAdapter = new MySqlDataAdapter(sqlCommand);
+                sqlAdapter.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ExecuteSQL(string command)
+        {
+            try
+            {
+                sqlConnect.Open();
+                sqlCommand = new MySqlCommand(command, sqlConnect);
+                sqlDataReader = sqlCommand.ExecuteReader();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+            finally
+            {
+                sqlConnect.Close();
+                updateDGV1();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            updateDGV1();
+        }
+
+        private void btn1_Click_1(object sender, EventArgs e)
+        {
+
+            string id = tbox1.Text;
+            string nation = tbox2.Text;
+            string abv = tbox3.Text;
+            string command = $"insert into nationality values ('{id}' ,'{abv}', '{nation}', 0";
+            ExecuteSQL(command);
+        }
+
+        private void dgv_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string id = dgv.CurrentRow.Cells[0].Value.ToString();
+            tbox4.Text = id;
+            tbox5.Text = id;
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbox4.Text))
+            {
+                MessageBox.Show("ID Textbox must not be empty");
+            }
+            else
+            {
+                string id = tbox4.Text;
+                string command = $"delete from nationality where nationality_id = '{id}'";
+                ExecuteSQL(command);
+            }
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            string id = tbox5.Text;
+            string nation = tbox6.Text;
+            string abv = tbox7.Text;
+            string command = $"update nationality set `abv` = '{abv}', `nation` = '{nation}' where` nationality_id` = '{id}'";
+            ExecuteSQL(command);
+            updateDGV1();
+        }
+    }
+}
