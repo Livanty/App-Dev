5ff74b9c424ede4b6ec7d272a6f36bb4b9617621
Livanty <121992352+Livanty@users.noreply.github.com>
2023-05-20 21:32:39 +0700

    CA/THA_W12_Livanty_ED

---
+++ Pratikum_W12/Pratikum_W12/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace Pratikum_W12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.txt5 = new System.Windows.Forms.TextBox();
+            this.txt6 = new System.Windows.Forms.TextBox();
+            this.dtp = new System.Windows.Forms.DateTimePicker();
+            this.cmb1 = new System.Windows.Forms.ComboBox();
+            this.cmb2 = new System.Windows.Forms.ComboBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.cmb3 = new System.Windows.Forms.ComboBox();
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.dgv2 = new System.Windows.Forms.DataGridView();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.cmb4 = new System.Windows.Forms.ComboBox();
+            this.dgv3 = new System.Windows.Forms.DataGridView();
+            this.btn3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Team Number:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Height:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Position:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Nationality: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 228);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Weight:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 258);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Birthdate: ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 287);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(103, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Team Name: ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(13, 322);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(81, 20);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Player ID: ";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(138, 63);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 26);
+            this.txt1.TabIndex = 9;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(138, 95);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 26);
+            this.txt2.TabIndex = 10;
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(138, 158);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(100, 26);
+            this.txt3.TabIndex = 11;
+            // 
+            // txt4
+            // 
+            this.txt4.Location = new System.Drawing.Point(138, 190);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(100, 26);
+            this.txt4.TabIndex = 12;
+            // 
+            // txt5
+            // 
+            this.txt5.Location = new System.Drawing.Point(138, 222);
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(100, 26);
+            this.txt5.TabIndex = 13;
+            // 
+            // txt6
+            // 
+            this.txt6.Location = new System.Drawing.Point(138, 316);
+            this.txt6.Name = "txt6";
+            this.txt6.Size = new System.Drawing.Size(100, 26);
+            this.txt6.TabIndex = 15;
+            // 
+            // dtp
+            // 
+            this.dtp.Location = new System.Drawing.Point(138, 251);
+            this.dtp.Name = "dtp";
+            this.dtp.Size = new System.Drawing.Size(196, 26);
+            this.dtp.TabIndex = 16;
+            // 
+            // cmb1
+            // 
+            this.cmb1.FormattingEnabled = true;
+            this.cmb1.Location = new System.Drawing.Point(138, 126);
+            this.cmb1.Name = "cmb1";
+            this.cmb1.Size = new System.Drawing.Size(121, 28);
+            this.cmb1.TabIndex = 17;
+            // 
+            // cmb2
+            // 
+            this.cmb2.FormattingEnabled = true;
+            this.cmb2.Location = new System.Drawing.Point(138, 282);
+            this.cmb2.Name = "cmb2";
+            this.cmb2.Size = new System.Drawing.Size(121, 28);
+            this.cmb2.TabIndex = 18;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(79, 368);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 19;
+            this.btn1.Text = "Insert";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // cmb3
+            // 
+            this.cmb3.FormattingEnabled = true;
+            this.cmb3.Location = new System.Drawing.Point(379, 13);
+            this.cmb3.Name = "cmb3";
+            this.cmb3.Size = new System.Drawing.Size(121, 28);
+            this.cmb3.TabIndex = 20;
+            this.cmb3.SelectedIndexChanged += new System.EventHandler(this.cmb3_SelectedIndexChanged);
+            // 
+            // dgv1
+            // 
+            this.dgv1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgv1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Location = new System.Drawing.Point(379, 48);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.RowHeadersWidth = 62;
+            this.dgv1.RowTemplate.Height = 28;
+            this.dgv1.Size = new System.Drawing.Size(286, 168);
+            this.dgv1.TabIndex = 21;
+            // 
+            // dgv2
+            // 
+            this.dgv2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgv2.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgv2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv2.Location = new System.Drawing.Point(379, 228);
+            this.dgv2.Name = "dgv2";
+            this.dgv2.RowHeadersWidth = 62;
+            this.dgv2.RowTemplate.Height = 28;
+            this.dgv2.Size = new System.Drawing.Size(286, 214);
+            this.dgv2.TabIndex = 22;
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(473, 471);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 23;
+            this.btn2.Text = "Update";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // cmb4
+            // 
+            this.cmb4.FormattingEnabled = true;
+            this.cmb4.Location = new System.Drawing.Point(709, 13);
+            this.cmb4.Name = "cmb4";
+            this.cmb4.Size = new System.Drawing.Size(121, 28);
+            this.cmb4.TabIndex = 24;
+            this.cmb4.SelectedIndexChanged += new System.EventHandler(this.cmb4_SelectedIndexChanged);
+            // 
+            // dgv3
+            // 
+            this.dgv3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv3.Location = new System.Drawing.Point(709, 48);
+            this.dgv3.Name = "dgv3";
+            this.dgv3.RowHeadersWidth = 62;
+            this.dgv3.RowTemplate.Height = 28;
+            this.dgv3.Size = new System.Drawing.Size(369, 394);
+            this.dgv3.TabIndex = 25;
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(861, 471);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 23);
+            this.btn3.TabIndex = 26;
+            this.btn3.Text = "Delete";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1107, 516);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.dgv3);
+            this.Controls.Add(this.cmb4);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.dgv2);
+            this.Controls.Add(this.dgv1);
+            this.Controls.Add(this.cmb3);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.cmb2);
+            this.Controls.Add(this.cmb1);
+            this.Controls.Add(this.dtp);
+            this.Controls.Add(this.txt6);
+            this.Controls.Add(this.txt5);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.TextBox txt5;
+        private System.Windows.Forms.TextBox txt6;
+        private System.Windows.Forms.DateTimePicker dtp;
+        private System.Windows.Forms.ComboBox cmb1;
+        private System.Windows.Forms.ComboBox cmb2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.ComboBox cmb3;
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.DataGridView dgv2;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.ComboBox cmb4;
+        private System.Windows.Forms.DataGridView dgv3;
+        private System.Windows.Forms.Button btn3;
+    }
+}
+
---
+++ Pratikum_W12/Pratikum_W12/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace Pratikum_W12
+{
+    public partial class Form1 : Form
+    {
+
+        MySqlConnection sqlConnect;
+        MySqlCommand sqlCommand;
+        MySqlDataAdapter sqlAdapter;
+        MySqlDataReader sqlDataReader;
+        string connectionString;
+        string sqlQuery;
+        DataTable dt = new DataTable();
+        DataTable dt2 = new DataTable();
+        DataTable dt3 = new DataTable();
+        DataTable dt4 = new DataTable();
+        DataTable dt5 = new DataTable();
+        DataTable dt6 = new DataTable();
+        DataTable dt7 = new DataTable();
+        public Form1()
+        {
+            try
+            {
+                connectionString = "server=localhost;uid=root;pwd=0mR0Nh3Mc4b!;database=premier_league";
+                sqlConnect = new MySqlConnection(connectionString);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            InitializeComponent();
+        }
+
+
+        private void updateDGV3()
+        {
+            dt7.Clear();
+            try
+            {
+                sqlQuery = $"select player_name, n.nation, p.team_number, p.height, p.weight, p.birthdate\r\nfrom player p, nationality n\r\nwhere team_id = '{cmb4.SelectedValue.ToString()}' and status = '1';";
+                sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+                sqlAdapter = new MySqlDataAdapter(sqlCommand);
+                sqlAdapter.Fill(dt7);
+                dgv3.DataSource = dt7;
+              
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void updateDGV2()
+        {
+            dt5.Clear();         
+            try
+            {
+                sqlQuery = $"select m.manager_name, t.team_name, m.birthdate, n.nation, m.manager_id \r\nfrom manager m, team t, nationality n \r\nwhere m.working = '0'  and n.nationality_id = m.nationality_id ;";
+                sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+                sqlAdapter = new MySqlDataAdapter(sqlCommand);
+                sqlAdapter.Fill(dt5);
+                dgv2.DataSource = dt5;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            dgv2.Columns[4].Visible = false;
+        }
+
+        private void updateDGV1()
+        {
+            dt4.Clear();
+            try
+            {
+                sqlQuery = $"select m.manager_name, t.team_name, m.birthdate, n.nation from manager m, team t, nationality n where m.working = '1' and m.manager_id = t.manager_id and n.nationality_id = m.nationality_id and team_id = '{cmb3.SelectedValue}';";
+                sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+                sqlAdapter = new MySqlDataAdapter(sqlCommand);
+                sqlAdapter.Fill(dt4);
+                dgv1.DataSource = dt4;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ExecuteSQL(string command)
+        {
+            try
+            {
+                sqlConnect.Open();
+                sqlCommand = new MySqlCommand(command, sqlConnect);
+                sqlDataReader = sqlCommand.ExecuteReader();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+            finally
+            {
+                sqlConnect.Close();               
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sqlQuery = $"select * from nationality;";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dt);
+            cmb1.DisplayMember = "nation";
+            cmb1.ValueMember = "nationality_id";
+            cmb1.DataSource = dt;
+
+            sqlQuery = $"select * from team;";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dt2);
+            cmb2.DisplayMember = "team_name";
+            cmb2.ValueMember = "team_id";
+            cmb2.DataSource = dt2;
+
+            sqlQuery = $"select * from team;";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dt3);
+            cmb3.DisplayMember = "team_name";
+            cmb3.ValueMember = "team_id";
+            cmb3.DataSource = dt3;
+
+            sqlQuery = $"select * from team;";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dt6);
+            cmb4.DisplayMember = "team_name";
+            cmb4.ValueMember = "team_id";
+            cmb4.DataSource = dt6;
+
+            //sqlQuery = $"select m.manager_name, t.team_name, m.birthdate, n.nation from manager m, team t, nationality n where m.working = '1' and m.manager_id = t.manager_id and n.nationality_id = m.nationality_id and team_id = '{cmb3.SelectedValue}';";
+            //sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            //sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            //sqlAdapter.Fill(dt7);
+            //dgv3.DataSource = dt7;
+
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            string name = txt1.Text;
+            string number = txt2.Text;
+            string position = txt3.Text;
+            string height = txt4.Text;
+            string weight = txt5.Text;
+            string playerid = txt6.Text;
+           
+            string tgl = dtp.Value.ToString("yyyy-MM-dd");
+            string command = $"insert into player values ('{playerid}' ,'{number}', '{name}', '{cmb1.SelectedValue}', '{position}', '{height}', '{weight}', '{tgl}', '{cmb2.SelectedValue}', '1', '0');" ;
+            ExecuteSQL(command);
+            txt1.Clear();
+            txt2.Clear();
+            txt3.Clear();
+            txt4.Clear();
+            txt5.Clear();
+            txt6.Clear();
+        }
+
+       
+        private void cmb3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dt5.Clear();
+            dt4.Clear();
+            dt5 = new DataTable();
+            dt4 = new DataTable();
+
+            sqlQuery = $"select m.manager_name, t.team_name, m.birthdate, n.nation from manager m, team t, nationality n where m.working = '1' and m.manager_id = t.manager_id and n.nationality_id = m.nationality_id and team_id = '{cmb3.SelectedValue}';";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dt4);
+            dgv1.DataSource = dt4;
+
+           
+            sqlQuery = $"select m.manager_name, m.birthdate, n.nation, m.manager_id \r\nfrom manager m, nationality n \r\nwhere m.working = '0'  and n.nationality_id = m.nationality_id ;";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dt5);
+            dgv2.DataSource = dt5;
+            dgv2.Columns[3].Visible = false;
+        }
+        private void btn2_Click(object sender, EventArgs e)
+        {
+           
+            if (dgv1.CurrentCell != null && dgv1.CurrentCell != null) // Pastikan ada sel yang saat ini dipilih
+            {
+
+                int rowIndex = dgv2.CurrentCell.RowIndex;
+                DataGridViewRow row = dgv2.Rows[rowIndex];
+                string value = row.Cells[0].Value.ToString();
+                string valuess = row.Cells[3].Value.ToString();
+                string command = $"update manager\r\nset working = '1' \r\nwhere manager_name = '{value}';";
+                string coba = $"update team\r\nset manager_id = '{valuess}' \r\nwhere team_id = '{cmb3.SelectedValue}';";
+                ExecuteSQL(command);
+                ExecuteSQL(coba);
+                updateDGV2();
+
+                int rowIndex1 = dgv1.CurrentCell.RowIndex;
+                DataGridViewRow row1 = dgv1.Rows[rowIndex1];
+                string value1 = row1.Cells[0].Value.ToString();
+                // MessageBox.Show(value.ToString());
+                string commands = $"update manager\r\nset working = '0' \r\nwhere manager_name = '{value1}';";
+                ExecuteSQL(commands);
+                updateDGV1();
+            }
+            dt5.Clear();
+            dt4.Clear();
+            dt5 = new DataTable();
+            dt4 = new DataTable();
+
+            sqlQuery = $"select m.manager_name, t.team_name, m.birthdate, n.nation from manager m, team t, nationality n where m.working = '1' and m.manager_id = t.manager_id and n.nationality_id = m.nationality_id and team_id = '{cmb3.SelectedValue}';";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dt4);
+            dgv1.DataSource = dt4;
+
+
+            sqlQuery = $"select m.manager_name, m.birthdate, n.nation, m.manager_id \r\nfrom manager m, nationality n \r\nwhere m.working = '0'  and n.nationality_id = m.nationality_id ;";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dt5);
+            dgv2.DataSource = dt5;
+            dgv2.Columns[3].Visible = false;
+        }
+        private void btn3_Click(object sender, EventArgs e)
+        {
+
+            if (dgv3.Rows.Count > 12)
+            {
+                int rowIndex = dgv3.CurrentCell.RowIndex;
+                DataGridViewRow row = dgv3.Rows[rowIndex];
+                string value = row.Cells[0].Value.ToString();
+            //    MessageBox.Show(value.ToString());
+
+                string command = $"update player set status = '0' where player_name = '{value}';";
+                ExecuteSQL(command);
+                updateDGV3();
+
+                dt7.Clear();
+                dt7 = new DataTable();
+                sqlQuery = $"select player_name, n.nation, p.team_number, p.height, p.weight, p.birthdate from player p left join nationality n on p.nationality_id = n.nationality_id where team_id = '{cmb4.SelectedValue}' and status = '1';";
+                sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+                sqlAdapter = new MySqlDataAdapter(sqlCommand);
+                sqlAdapter.Fill(dt7);
+                dgv3.DataSource = dt7;
+            }
+            else
+            {
+                MessageBox.Show("Min 11");
+            }
+            
+        }
+        private void cmb4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dt7.Clear();
+            dt7 = new DataTable();
+            sqlQuery = $"select player_name, n.nation,p.playing_pos, p.team_number, p.height, p.weight, p.birthdate from player p left join nationality n on p.nationality_id = n.nationality_id where team_id = '{cmb4.SelectedValue}' and status = '1';";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dt7);
+            dgv3.DataSource = dt7;
+        }
+     
+    }
+}
